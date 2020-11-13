b20cc038128b7949fe8e55d5a751c65aa0594d0f
Brad Johansen <brad.d.johansen@gmail.com>
2020-11-13 14:09:13 -0600

    foundation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,9 +9,13 @@ Home
 @endsection
 
 @section('content')
-    <div class="container">
-        <div class="jumbotron">
-        </div>
+    <div class="jumbotron text-center">
+      <h1 class="display-4">Nerds Rule</h1>
+      <p class="lead">Remember there is no place like 127.0.0.1!</p>
+      <hr>
+      <p>More to come soon</p>
+      <a class="btn btn-primary btn-lg" href="#" role="button">Read More</a>
+    </div>
         <div class="row">
             <div class="col-6">stuff</div>
             <div class="col-6">more stuff</div>
